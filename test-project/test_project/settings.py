4e27f55ba276e16ce406353540b00b99ee469e0e
Hynek Cernoch <hynek@sdb.cz>
2013-05-22 23:37:44 +0200

    Moved the test project to a standard place.

--- test-project/test_project/settings.py
+++ test-project/test_project/settings.py
@@ -1,4 +1,4 @@
-# Django settings for test_app project.
+# Django settings for test_project project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'test_app.urls'
+ROOT_URLCONF = 'test_project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'test_app.wsgi.application'
+WSGI_APPLICATION = 'test_project.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
