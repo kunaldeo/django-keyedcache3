476325bcec5cf8bc229f70033c368927347fd1cf
Chris Moffitt <chris@moffitts.net>
2014-04-24 19:15:09 -0500

    Fix #13 Also bump rev.

--- keyedcache/urls.py
+++ keyedcache/urls.py
@@ -2,7 +2,12 @@
 URLConf for Caching app
 """
 
-from django.conf.urls import patterns
+try:
+	from django.conf.urls import patterns
+except ImportError:
+	from django.conf.urls.defaults import patterns
+
+
 urlpatterns = patterns('keyedcache.views',
     (r'^$', 'stats_page', {}, 'keyedcache_stats'),
     (r'^view/$', 'view_page', {}, 'keyedcache_view'),
