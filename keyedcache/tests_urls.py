e859aa8f5d6e9db09245e9bf72eb4a613a8470d4
Kunal Deo <kunaldeo@gmail.com>
2016-02-05 03:15:07 +0530

    Import Style change in dajngo.contrib.auth.views.login

--- keyedcache/tests_urls.py
+++ keyedcache/tests_urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
+import django.contrib.auth.views
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^cache/', include('keyedcache.urls')),
-    url(r'^accounts/login/', login, {'template_name': 'admin/login.html'}),
+    url(r'^accounts/login/', django.contrib.auth.views.login, {'template_name': 'admin/login.html'}),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- test-project/test_project/urls.py
+++ test-project/test_project/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as authviews
+import django.contrib.auth.views
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^cache/', include('keyedcache.urls')),
-    url(r'^accounts/login/', authviews.login, {'template_name': 'admin/login.html'}),
+    url(r'^accounts/login/',  django.contrib.auth.views.login, {'template_name': 'admin/login.html'}),
     url(r'^admin/', include(admin.site.urls)),
 ]
